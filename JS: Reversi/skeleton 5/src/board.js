9d93fd620eb37f4c72dad5c301d9fca938373508
Sebastian Sanchez <sebastian@artxcode.io>
2021-04-15 14:08:06 -0400

    finished piece, started on board

--- JS: Reversi/skeleton 5/src/board.js
+++ JS: Reversi/skeleton 5/src/board.js
@@ -9,7 +9,19 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
-}
+  let grid = [];
+  for (i = 0; i < 8; i++) {
+    let row = new Array(8);
+      grid.push(row);
+  }
+
+  grid[3][4] = new Piece("black");
+  grid[4][3] = new Piece("black");
+  grid[3][3] = new Piece("white");
+  grid[4][4] = new Piece("white");
+
+return grid;
+};
 
 /**
  * Constructs a Board with a starting grid set up.
